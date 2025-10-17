771573fb8558640a60156b9b0d10d782ee27be31
acshacsh <54509513+sunilsah0@users.noreply.github.com>
2025-10-17 16:22:25 +0200

    Add Features model using Pydantic

---
+++ app/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class Features(BaseModel):
+    age: float
+    sex: float
+    bmi: float
+    bp: float
+    s1: float
+    s2: float
+    s3: float
+    s4: float
+    s5: float
+    s6: float
